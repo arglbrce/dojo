72af46711218e351cea37293eece52d4680287c3
45773653320 <allan.lima@serpro.gov.br>
2019-06-18 14:03:11 -0300

    matriz espiral

--- matriz_espiral.py
+++ matriz_espiral.py
@@ -11,6 +11,13 @@ Variação do problema: retorne a espiral no sentido anti-horário.
 
 Este problema é mais fácil de ser compreendido através de exemplos:
 
+Entrada: 2 2
+
+Saída:
+
+ 1  2
+ 4  3
+
 Entrada: 3 4
 
 Saída:
@@ -33,3 +40,31 @@ Saída:
 
 
 """
+
+import unittest
+
+def matrix(row, col):
+    numbers = []
+    for num in range(1,row*col+1):
+        numbers.append(num)
+
+    if row == 1 and col == 1:
+        return [[1]]
+    elif row == 1 and col == 2:
+        return [[1,2]]
+    elif row == 2 and col == 2:
+        numbers[:2],numbers[2:]
+        return [[1,2],[4,3]]
+
+class MatrizEspiralTest(unittest.TestCase):
+    def test_matrix_1_1(self):
+        self.assertEqual(matrix(1,1),[[1]])
+
+    def test_matrix_1_2(self):
+        self.assertEqual(matrix(1,2),[[1,2]])
+
+    def test_matrix_2_2(self):
+        self.assertEqual(matrix(2,2),[[1,2],[4,3]])
+
+if __name__ == "__main__":
+    unittest.main()
